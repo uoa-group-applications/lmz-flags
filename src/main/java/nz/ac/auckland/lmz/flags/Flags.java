52990c670717dec62d40e5c08cab9a7a6bff1f85
Richard Vowles <richard@bluetrainsoftware.com>
2013-11-20 15:32:06 +1300

    support LMZ specific devmode flag

---
+++ src/main/java/nz/ac/auckland/lmz/flags/Flags.java
@@ -0,0 +1,19 @@
+package nz.ac.auckland.lmz.flags;
+
+/**
+ * @author: Richard Vowles - https://plus.google.com/+RichardVowles
+ */
+public enum Flags {
+	// are we in dev mode. Normally set by a test.
+	DEVMODE("lmz.devmode");
+
+	private final String property;
+
+	public boolean on() {
+		return "true".equalsIgnoreCase(System.getProperty(property));
+	}
+
+	private Flags(String property) {
+		this.property = property;
+	}
+}
